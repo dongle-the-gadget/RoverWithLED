6d48eb586c75c708f526f532f1467d9150df22ff
superkid200 <minh306@outlook.com.vn>
2017-07-25 23:14:09 +0700

    LED Car

---
+++ LEDCar.ino
@@ -0,0 +1,25 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 5;
+int button = 6;
+
+void setup() {
+  pinMode(button,INPUT);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+    }
+
+void loop() {
+int ButtonStatus = digitalRead(button);
+if (ButtonStatus == HIGH)
+{
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+} else {
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+}
+    }
